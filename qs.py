1c3e7be9978ee5ef3c64fa8641c9c3bd16523ddb
Jordan <jtilbert89@gmail.com>
2022-01-26 10:40:48 -0700

    width function

--- qs.py
+++ qs.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
+from sklearn.linear_model import LinearRegression
 
 
 class FractionalTransport:
@@ -26,31 +27,39 @@ class FractionalTransport:
         else:
             self.d_df = d_df[d_df['stream_id'] == stream_id]
 
-        # use function with hydraulic geometry data to come up with relationship width as function of depth
+    # use function with hydraulic geometry data to come up with relationship width as function of depth
+    def calc_width_params(self):
+        x_data = np.array(self.geom_df['h']).reshape(-1,1)
+        y_data = np.array(self.geom_df['w'])
+        lr = LinearRegression()
+        lr.fit(x_data, y_data)
+        print('depth-width R2: ', str(lr.score(x_data, y_data)))
 
-        # use function to find D50 and Fi for each size class i from grain size data
+        return lr.coef_, lr.intercept_
 
-        # UPDATE THESE FUNCTIONS FOR USE IN THIS CLASS
-        def err(v, d, s, chan_type, coef, intercept, q_obs):
-            h = d ** 0.25 * ((v ** 1.5 / (9.81 * s) ** 0.75) / 22.627)
-            if chan_type == 'confined':
-                w = coef * np.log(h) + intercept
-            elif chan_type == 'floodplain':
-                w = coef * np.exp(intercept * h)
-            q_pred = v * h * w
-            err = (q_obs - q_pred) ** 2
+    # use function to find D50 and Fi for each size class i from grain size data
+
+    # UPDATE THESE FUNCTIONS FOR USE IN THIS CLASS
+    def err(v, d, s, chan_type, coef, intercept, q_obs):
+        h = d ** 0.25 * ((v ** 1.5 / (9.81 * s) ** 0.75) / 22.627)
+        if chan_type == 'confined':
+            w = coef * np.log(h) + intercept
+        elif chan_type == 'floodplain':
+            w = coef * np.exp(intercept * h)
+        q_pred = v * h * w
+        err = (q_obs - q_pred) ** 2
 
             return err
 
-        def get_gs_ratio(d, S, chan_type, coef, intercept, Q, D50):
-            v0 = 5
+    def get_gs_ratio(d, S, chan_type, coef, intercept, Q, D50):
+        v0 = 5
 
-            res = minimize(err, v0, args=(d, S, chan_type, coef, intercept, Q))
-            h_adj = d ** 0.25 * ((res.x[0] ** 1.5 / (9.81 * S) ** 0.75) / 22.627)
-            tau_g_star = (9180 * h_adj * S) / (1650 * 9.81 * d)
-            ratio = tau_g_star / (0.038 * (d / D50) ** -0.65)  #
-            if ratio <= 0:
-                ratio = 0
+        res = minimize(err, v0, args=(d, S, chan_type, coef, intercept, Q))
+        h_adj = d ** 0.25 * ((res.x[0] ** 1.5 / (9.81 * S) ** 0.75) / 22.627)
+        tau_g_star = (9180 * h_adj * S) / (1650 * 9.81 * d)
+        ratio = tau_g_star / (0.038 * (d / D50) ** -0.65)  #
+        if ratio <= 0:
+            ratio = 0
 
-            return ratio, res.x[0], h_adj
+        return ratio, res.x[0], h_adj
 
