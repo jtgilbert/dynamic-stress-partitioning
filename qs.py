30568b5442ed80f36b131ef00cefcbff5491025b
Jordan <jtilbert89@gmail.com>
2022-03-11 15:36:49 -0700

    last functions

--- qs.py
+++ qs.py
@@ -10,11 +10,11 @@ class FractionalTransport:
     Create an instance of fractional transport calculations for a given stream reach.
     """
 
-    def __init__(self, stream_id):
+    def __init__(self, stream_id, reach_slope):
 
         geom_df = pd.read_csv('Input_data/hydraulic_geometry.csv')
         d_df = pd.read_csv('Input_data/grain_size.csv')
-        q_df = pd.read_csv('Input_data/discharge.csv') # do I make this a table you add to, ie has stream name, or you just fill out every time...?
+        # q_df = pd.read_csv('Input_data/discharge.csv') # do I make this a table you add to, ie has stream name, or you just fill out every time...?
 
         # pull the selected stream id from the hydraulic geometry csv
         if stream_id not in geom_df.stream_id.unique():
@@ -28,30 +28,45 @@ class FractionalTransport:
         else:
             self.d_df = d_df[d_df['stream_id'] == stream_id]
 
+        self.q_df = pd.read_csv('Input_data/discharge.csv')
+
+        #
+        self.s = reach_slope
+
+        # Calculate grain size characteristics
         self.d50 = np.percentile(self.d_df, 50)
         self.d84 = np.percentile(self.d_df, 84)
         self.d_fractions = self.grain_sizes()
+
+        # To be able to calculate depth for each discharge (used to get V0)
+        self.h_coef, self.h_intercept = self.h_from_q_params()
+
+        # to find width for any depth
+        self.w_coef, self.w_intercept = self.width_from_depth_params()
+
+        # set up output table - columns:
 
     def grain_sizes(self):
         # set up output dictionary which associates a fraction value with each size range
         d_dict = {}
 
         # set up the size ranges
-        intervals = [[0,0.5],[0.5,1],[1,2],[2,4],[4,6],[6,8],[8,12],[12,16],[16,24],[24,32],[32,48],[48,64],[64,96],[96,128],
-                     [128,192],[192,256]]
+        intervals = [[0,0.5],[0.5,1],[1,2],[2,4],[4,6],[6,8],[8,12],[12,16],[16,24],[24,32],[32,48],[48,64],[64,96],
+                     [96,128],[128,192],[192,256]]
 
         # append values to the dictionary
         for i in intervals:
             count = 0
-            for d in self.d_df:
-                if i[0] < d <= i[1]:
+            for d in self.d_df.index:
+                if i[0] < (self.d_df.loc[d, 'D']*1000) <= i[1]:
                     count += 1
-            d_dict[str(i)] = count/len(self.d_df)
+            d_dict[i[0]] = count/len(self.d_df)
 
-        return d_dict # dictionary {half phi: fraction}
+        return d_dict  # dictionary {half phi: fraction}
 
     # use function with hydraulic geometry data to come up with relationship width as function of depth
-    def calc_width_params(self):
+    # this (as is now) assumes a linear relationship between width and depth (instead of choosing between log and exp)
+    def width_from_depth_params(self): # maybe change to do two types of regression compare r2 and use the better
         x_data = np.array(self.geom_df['h']).reshape(-1,1)
         y_data = np.array(self.geom_df['w'])
         lr = LinearRegression()
@@ -60,34 +75,73 @@ class FractionalTransport:
 
         return lr.coef_, lr.intercept_
 
-    def ferguson_vpe(self, h, s):
-        
-        v_est = ((6.5*(h/self.d84)) / (((h/self.d84)**(5/3)+(6.5/2.5)**2)**0.5))*(9.81*h*s)**0.5
+    def calc_w_from_h(self, h):
+
+        return self.w_coef*h+self.w_intercept
+
+    def ferguson_vpe(self, h):
+
+        v_est = ((6.5*(h/self.d84)) / (((h/self.d84)**(5/3)+(6.5/2.5)**2)**0.5))*(9.81*h*self.s)**0.5
         return v_est
 
-    # use function to find D50 and Fi for each size class i from grain size data
+    def h_from_q_params(self):
+        q = np.array(self.geom_df['Q']).reshape(-1,1)
+        h = np.array(10**self.geom_df['h'])
+        lr = LinearRegression()
+        lr.fit(q, h)
+        print('discharge-depth r2: ', str(lr.score(q, h)))
+
+        return lr.coef_, lr.intercept_
+
+    def calc_h_from_q(self, q):
+
+        return np.log(self.h_coef*q+self.h_intercept)
+
+    def tau_gc_star_i(self, d):
+
+        return 0.038*(d/self.d50)**-0.65
+
+    def tau_g_star_i(self, h_i, d):
+
+        return (9810*h_i*self.s)/(1.65*9.81*d)
 
     # UPDATE THESE FUNCTIONS FOR USE IN THIS CLASS
-    def err(v, d, s, chan_type, coef, intercept, q_obs):
-        h = d ** 0.25 * ((v ** 1.5 / (9.81 * s) ** 0.75) / 22.627)
-        if chan_type == 'confined':
-            w = coef * np.log(h) + intercept
-        elif chan_type == 'floodplain':
-            w = coef * np.exp(intercept * h)
+    def err(self, v, d, q_obs):
+        h = d ** 0.25 * ((v ** 1.5 / (9.81 * self.s) ** 0.75) / 22.627)
+        w = self.calc_w_from_h(h)
         q_pred = v * h * w
         err = (q_obs - q_pred) ** 2
 
-            return err
+        return err
 
-    def get_gs_ratio(d, S, chan_type, coef, intercept, Q, D50):
-        v0 = 5
+    def calc_hi(self, d, q, h):
+        v0 = self.ferguson_vpe(h)*2
 
-        res = minimize(err, v0, args=(d, S, chan_type, coef, intercept, Q))
-        h_adj = d ** 0.25 * ((res.x[0] ** 1.5 / (9.81 * S) ** 0.75) / 22.627)
-        tau_g_star = (9180 * h_adj * S) / (1650 * 9.81 * d)
-        ratio = tau_g_star / (0.038 * (d / D50) ** -0.65)  #
-        if ratio <= 0:
-            ratio = 0
+        res = minimize(self.err, v0, args=(d, q))
+        h_adj = d ** 0.25 * ((res.x[0] ** 1.5 / (9.81 * self.s) ** 0.75) / 22.627)
 
-        return ratio, res.x[0], h_adj
+        return h_adj
+
+    def find_fractional_transport(self):
+
+        for i in self. q_df.index:
+            q = self.q_df.loc[i, 'Q']
+            h = self.calc_h_from_q(q)
+            for d in self.d_fractions.keys():
+                tau_gc_star_i = self.tau_gc_star_i(d)
+                hi = self.calc_hi(d, q, h)
+                tau_g_star_i = self.tau_g_star_i(hi, d)
+                ratio = tau_g_star_i/tau_gc_star_i
+                if ratio < 0:
+                    ratio = 0
+                if ratio < 2:
+                    wi_star = 0.0008*ratio**7.5
+                elif ratio >= 2:
+                    wi_star = 14*(1-(1.11/ratio**0.8))**4.5
+
+                # convert from wi_star to qs
+                
+
+
+
 
