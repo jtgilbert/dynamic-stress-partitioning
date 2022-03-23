e68fdc9f42024253640f6f90657ef37ee659813c
Jordan <jtilbert89@gmail.com>
2022-03-23 17:49:16 -0600

    fixed conversion to yield and made command line compatible

--- DSP_Fractional_Transport/run_qs.py
+++ DSP_Fractional_Transport/run_qs.py
@@ -1,8 +1,8 @@
-import qs
+from DSP_Fractional_Transport import qs
 
 stream_id = 'Blodgett'
-reach_slope = 0.02
+reach_slope = 0.019
 discharge_interval = 900
 # minimum_fraction = 0  # the default value is 0.01 but it can be changed and added as a param below.
 
-qs.FractionalTransport(stream_id, reach_slope, discharge_interval)
+qs.FractionalTransport(stream_id, reach_slope, discharge_interval, minimum_fraction=0.005)
